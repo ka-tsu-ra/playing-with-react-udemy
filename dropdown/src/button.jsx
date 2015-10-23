f735979fce345fcc12034d165e9f191bd3a24306
Jones <kirsten.jones@FT-MW4599.local>
2015-10-23 09:25:23 +0100

    Adds onclick event inside button component

--- dropdown/src/button.jsx
+++ dropdown/src/button.jsx
@@ -1,8 +1,11 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  handleClick: function() {
+    alert('button clicked');
+  },
   render: function() {
-    return <button className={"btn " + this.props.className} type="button">
+    return <button onClick={this.handleClick} className={"btn " + this.props.className} type="button">
       {this.props.title}
       <span className={this.props.subTitleClassName}>{this.props.subTitle}</span>
     </button>
--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -5,6 +5,7 @@ var Button = require('./button');
 // This component should know when to show the list based on when the user clicks on a button.
 
 module.exports = React.createClass({
+  
   render: function() {
     return <div className="dropdown">
       <Button className="btn-default" title={this.props.title} subTitleClassName="caret" />
