1942cdb37fedf30dbf02b74967570435e169576c
Jones <kirsten.jones@FT-MW4599.local>
2015-10-23 11:28:27 +0100

    Adds comment explaining onClick and whenClicked and how it makes Button component more reusable

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -10,9 +10,19 @@ module.exports = React.createClass({
   },
   render: function() {
     return <div className="dropdown">
-      <Button whenClicked={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret" />
+      <Button
+        whenClicked={this.handleClick}
+        className="btn-default"
+        title={this.props.title}
+        subTitleClassName="caret"
+        />
     </div>
 
   }
 
 })
+
+// Button component onClick outcome is set to this.props.whenClicked, so when the Dropdown component renders a Button inside it,
+// you give it a whenClicked function and assign that to this.handleClick, which refers to the Dropdown component's hendleClikc function.
+// So any component that renders a Button inside it can decide what the Button's onClick outcome will be. Makes the Button component
+// reusable and adaptable, and you won't need to re-write any bit of Button again.
