72bed28dffea895e93198b54d8ef9e25c7d2e058
Jones <kirsten.jones@FT-MW4599.home>
2015-10-25 12:14:00 +0000

    Dropdown items have handleClick function. Just console.logging the item name for now

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -14,12 +14,15 @@ module.exports = React.createClass({
     // Creates an open object in the state of Dropdown. Now this.state.open exists and it's set to false initially.
   },
 
+  handleItemClick: function(item) {
+    console.log(item);
+  },
   // gets run only once, when the component gets rendered.
   render: function() {
     // items comes from the options object
     var list = this.props.items.map(function(item){
-      return <ListItem item={item} />
-    });
+      return <ListItem item={item} whenItemClicked={this.handleItemClick} />
+  }.bind(this));
     // builds up the list. Refers to items in the options variable in app.jsx which is passed into Dropdown when it's created, also in app.jsx.
     // maps over that list of items in var options and creates a ListItem component for each element where item (which is referring to this.props.item inside ListItem component) is equal to the item mapped over from the options object.
     // Probably building up an array. Then looks like in the render of Dropdown {list} between the <ul>s converts into the <li> elements. (??)
--- dropdown/src/list-item.jsx
+++ dropdown/src/list-item.jsx
@@ -1,8 +1,14 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  handleClick: function() {
+    this.props.whenItemClicked(this.props.item);
+  },
+
   render: function() {
-    return <li><a>{this.props.item}</a></li>
-    
+    return <li><a onClick={this.handleClick}>{this.props.item}</a></li>
   }
 });
+
+// could have put this.props.whenItemClicked directly into the render function for list-tem.
+// here abstracted it into handleClick function so you could have multiple different actions on the click event
